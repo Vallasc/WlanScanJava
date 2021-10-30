2b90c2d1883035481eccb08581cf80d11abb6f9b
Vallasc <giacomovallorani.97@gmail.com>
2021-10-31 00:19:01 +0200

    Initial commit

---
+++ src/main/java/io/github/vallasc/WlanScanner.java
@@ -0,0 +1,5 @@
+package io.github.vallasc;
+
+public interface WlanScanner {
+    public String scanNetworks();
+}
---
+++ src/main/java/io/github/vallasc/WlanScannerImpl.java
@@ -0,0 +1,9 @@
+package io.github.vallasc;
+
+public class WlanScannerImpl implements WlanScanner {
+    
+    public String scanNetworks(){
+        System.out.println("Hello");
+        return "Hello";
+    }
+}
