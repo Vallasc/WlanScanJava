8af6c60cd4c537b886cb9f3895da7d903cfdf3db
Vallasc <giacomovallorani.97@gmail.com>
2021-10-31 13:07:23 +0100

    Refactor

--- src/main/java/io/github/vallasc/WlanScannerImpl.java
+++
@@ -1,9 +0,0 @@
-package io.github.vallasc;
-
-public class WlanScannerImpl implements WlanScanner {
-    
-    public String scanNetworks(){
-        System.out.println("Hello");
-        return "Hello";
-    }
-}
