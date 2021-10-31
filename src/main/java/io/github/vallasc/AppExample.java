6f0df6448b551b3aeadf9a67131a551a1881efec
Vallasc <giacomovallorani.97@gmail.com>
2021-10-31 13:06:48 +0100

    Add example

---
+++ src/main/java/io/github/vallasc/AppExample.java
@@ -0,0 +1,22 @@
+package io.github.vallasc;
+
+import java.io.IOException;
+
+import io.github.vallasc.WlanScanner.OperatingSystemNotDefinedException;
+
+public class AppExample {
+
+    // mvn compile exec:java
+    public static void main( String[] args ) {
+        System.out.println( "WlanScanner" );
+        WlanScanner w = new WlanScanner();
+        try {
+            APInfo[] apList = w.scanNetworks();
+            for(APInfo ap : apList){
+                System.out.println(ap + "\n");
+            }
+        } catch (OperatingSystemNotDefinedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
